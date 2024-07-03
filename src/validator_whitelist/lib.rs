f9024e1e93306a50fd44ded1cc8d3614d383d660
johhonn <jjj.may377@gmail.com>
2024-07-03 10:15:53 -0400

    format

--- src/validator_whitelist/lib.rs
+++ src/validator_whitelist/lib.rs
@@ -1,30 +1,25 @@
-// 
+//
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
 #[ink::contract]
-mod validator_whitelist{
+mod validator_whitelist {
     use ink::{
-        env::{
-            debug_println,
-            DefaultEnvironment,
-         
-        },
+        env::{debug_println, DefaultEnvironment},
         prelude::{string::String, vec::Vec},
-      
         storage::Mapping,
     };
     use psp22::{PSP22Error, PSP22};
     use psp34::{Id, PSP34Error, PSP34};
-    use registry::{Registry,registry::RegistryError};
+    use registry::{registry::RegistryError, Registry};
 
     #[ink(storage)]
-    pub struct ValidatorWhitelist{
-        admin:AccountId,
-        registry:AccountId,
-        gov_nft:AccountId,
-        validators:Vec<AccountId>,
-        token_stake_amount:u128,
-        create_deposit:u128,                
+    pub struct ValidatorWhitelist {
+        admin: AccountId,
+        registry: AccountId,
+        gov_nft: AccountId,
+        validators: Vec<AccountId>,
+        token_stake_amount: u128,
+        create_deposit: u128,
     }
     #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
@@ -35,8 +30,7 @@ mod validator_whitelist{
         NFTError(PSP34Error),
         TokenError(PSP22Error),
     }
-    impl ValidatorWhitelist{
-       
+    impl ValidatorWhitelist {
         fn transfer_psp34(
             &self,
             from: &AccountId,
@@ -50,31 +44,38 @@ mod validator_whitelist{
             Ok(())
         }
         #[ink(constructor)]
-        pub fn new(
-            _admin:AccountId,
-            _registry:AccountId
-        ) -> Self {
-             Self {
-            admin:_admin,
-            registry:_registry,
-            gov_nft:AccountId,
-            validators:Vec::new(),
-            token_stake_amount:100000_u128,
-            create_deposit:100000_u128,        
+        pub fn new(_admin: AccountId, _registry: AccountId) -> Self {
+            Self {
+                admin: _admin,
+                registry: _registry,
+                gov_nft: AccountId,
+                validators: Vec::new(),
+                token_stake_amount: 100000_u128,
+                create_deposit: 100000_u128,
             }
         }
         #[ink(message)]
-        pub fn join_whitelist(&mut self)->Result<(),WhitelistError>{
+        pub fn join_whitelist(&mut self) -> Result<(), WhitelistError> {
             Ok(())
         }
         #[ink(message)]
-        pub fn add_validator(&mut self,validator:AccountId)->Result<(),WhitelistError>{
+        pub fn init_add_validator(&mut self, validator: AccountId) -> Result<(), WhitelistError> {
             Ok(())
         }
         #[ink(message)]
-        pub fn remove_validator(&mut self,validator:AccountId,slash:bool)->Result<(),WhitelistError>{
+        pub fn finalize_add_validator(
+            &mut self,
+            validator: AccountId,
+        ) -> Result<(), WhitelistError> {
+            Ok(())
+        }
+        #[ink(message)]
+        pub fn remove_validator(
+            &mut self,
+            validator: AccountId,
+            slash: bool,
+        ) -> Result<(), WhitelistError> {
             Ok(())
         }
     }
-  
 }
