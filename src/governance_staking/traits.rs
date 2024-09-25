54841e12423943094c136a3dc6da6e90f00b94c7
Brandon <brandon@kintsu.xyz>
2024-09-25 15:47:43 -0400

    remove governance code

--- src/governance_staking/traits.rs
+++
@@ -1,6 +0,0 @@
-use crate::staking::StakingError;
-#[ink::trait_definition]
-pub trait Staking{
-    #[ink(message)]
-    fn update_rewards_rate(&mut self,new_rate:u128) -> Result<(), StakingError>;
-}
