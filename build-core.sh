e17a1296454e507275fbcdcdc78133aaa6d456e8
Brandon <brandon@kintsu.xyz>
2024-10-21 14:20:53 -0400

    always build into development dir
    avoid accidents when $CHAIN is set

--- build-core.sh
+++ build-core.sh
@@ -5,10 +5,6 @@ set -eu
 CONTRACTS_DIR="./src" # Base contract directory
 DEPLOYMENTS_DIR="./deployments" # Output directory for build files
 
-# Default CHAIN to "development" if not set
-: "${CHAIN:=development}"
-echo "Using CHAIN: $CHAIN"
-
 # Copy command helper (cross-platform)
 CP_CMD=$(command -v cp &> /dev/null && echo "cp" || echo "copy")
 
@@ -20,9 +16,9 @@ do
   echo -e "\nBuilding '$CONTRACTS_DIR/$i/Cargo.toml'…"
   cargo +stable-2023-12-28 contract build --release --quiet --manifest-path $CONTRACTS_DIR/$i/Cargo.toml
 
-  echo "Copying build files to '$DEPLOYMENTS_DIR/$CHAIN/$i'…"
-  mkdir -p $DEPLOYMENTS_DIR/$CHAIN/$i
-  $CP_CMD ./target/ink/$i/$i.contract $DEPLOYMENTS_DIR/$CHAIN/$i/
-  $CP_CMD ./target/ink/$i/$i.wasm $DEPLOYMENTS_DIR/$CHAIN/$i/
-  $CP_CMD ./target/ink/$i/$i.json $DEPLOYMENTS_DIR/$CHAIN/$i/
+  echo "Copying build files to '$DEPLOYMENTS_DIR/development/$i'…"
+  mkdir -p $DEPLOYMENTS_DIR/development/$i
+  $CP_CMD ./target/ink/$i/$i.contract $DEPLOYMENTS_DIR/development/$i/
+  $CP_CMD ./target/ink/$i/$i.wasm $DEPLOYMENTS_DIR/development/$i/
+  $CP_CMD ./target/ink/$i/$i.json $DEPLOYMENTS_DIR/development/$i/
 done
