466889d3fdddfc64ae0e83c484af703b3db02935
Nimish Agrawal <realnimish@gmail.com>
2025-01-30 20:16:39 +0530

    fix: incorrect arguments passed in cross-contract call

--- src/governance_staking/lib.rs
+++ src/governance_staking/lib.rs
@@ -421,9 +421,7 @@ pub mod staking {
                 .exec_input(
                     ExecutionInput::new(ADD_SELECTOR)
                         .push_arg(admin)
-                        .push_arg(validator)
-                        .push_arg(pool_create_amount)
-                        .push_arg(existential_deposit),
+                        .push_arg(validator),
                 )
                 .transferred_value(transfer_amount)
                 .returns::<Result<AccountId, RuntimeError>>()
