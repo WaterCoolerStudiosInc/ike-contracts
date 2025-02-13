e4cd844bd24a11688b94baa76550fd4ff88125e6
Nimish Agrawal <realnimish@gmail.com>
2025-02-13 21:05:48 +0530

    fix: doc-tests

--- src/governance_council/lib.rs
+++ src/governance_council/lib.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 pub mod traits;
-pub use governance_council::CouncilRef;
+pub use crate::governance_council::CouncilRef;
 pub use traits::ICouncil;
 
 #[ink::contract]
